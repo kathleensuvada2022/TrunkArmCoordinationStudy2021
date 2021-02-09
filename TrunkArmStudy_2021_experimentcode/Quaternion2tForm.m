f3f321216bd50006fa14118464086b7b07af4d63
Kathleen Suvada <kathleensuvada2022@u.northwestern.edu>
2021-02-09 12:52:56 -0600

    Created function Quaternion2tForm
    for long way of converting to matlab2014. Verified with matlab2020 way quat2tform and got the same matrix

--- TrunkArmStudy_2021_experimentcode/Quaternion2tForm.m
+++ TrunkArmStudy_2021_experimentcode/Quaternion2tForm.m
@@ -10,65 +10,77 @@ function H = Quaternion2tForm(q)
 
 %% From quat2rotm
 % Validate the quaternions
- q = robotics.internal.validation.validateQuaternion(q, 'quat2rotm', 'q')
+%  q = robotics.internal.validation.validateQuaternion(q, 'quat2rotm', 'q')
+%  KCS 2.9.21 -- don't need this? function?
    
-    function numericMatrix = validateQuaternion(q, funcname, varname)
-%This function is for internal use only. It may be removed in the future.
-
-%VALIDATEQUATERNION Validate quaternion and return it as N-by-4 numeric matrix
-%   NUMERICMATRIX = VALIDATEQUATERNION(Q, FUNCNAME, VARNAME) validates
-%   whether the input Q represents a valid quaternion and returns the input
-%   quaternion, Q, as N-by-4 numeric matrix, NUMERICMATRIX, where each row
-%   is one quaternion. The input quaternion, Q, is expected to be either
-%   N-by-4 numeric matrix of row quaternions, or N-by-1 or 1-by-N vector of
-%   QUATERNION objects. FUNC_NAME and VAR_NAME are used in
-%   VALIDATEATTRIBUTES to construct the error id and message.
-
-%   Copyright 2017-2019 The MathWorks, Inc.
-
-%#codegen
-
-if (isa(q, 'quaternion'))
-        
-    % Extract quaternion parts
-    [w,x,y,z] = parts(q);
-    
-    % Validate that the input quaternion is non-empty and that the input is
-    % a vector. The validateattributes function is not supported for
-    % quaternion input. Therefore, the validateattributes is called on each
-    % part separately.
-    validateattributes(w, {'numeric'}, {'nonempty', 'vector'}, funcname, varname);
-    validateattributes(x, {'numeric'}, {'nonempty', 'vector'}, funcname, varname);
-    validateattributes(y, {'numeric'}, {'nonempty', 'vector'}, funcname, varname);
-    validateattributes(z, {'numeric'}, {'nonempty', 'vector'}, funcname, varname);
-    
-    % If the quaternion input is a row vector, transpose it to get a column
-    % vector
-    if (isrow(w))
-        w = w';
-        x = x';
-        y = y';
-        z = z';
-    end
-    
-    % Save quaternion parts as N-by-4 numeric matrix
-    numericMatrix = [w,x,y,z];
-    
-else
-    
-    % Validate that the input is N-by-4 numeric matrix
-    robotics.internal.validation.validateNumericMatrix(q, funcname, varname, ...
-        'ncols', 4);
-    
-    % Return the input as is
-    numericMatrix = q;
-    
-end
-
-end
+%     function numericMatrix = validateQuaternion(q, funcname, varname)
+% %This function is for internal use only. It may be removed in the future.
+% 
+% %VALIDATEQUATERNION Validate quaternion and return it as N-by-4 numeric matrix
+% %   NUMERICMATRIX = VALIDATEQUATERNION(Q, FUNCNAME, VARNAME) validates
+% %   whether the input Q represents a valid quaternion and returns the input
+% %   quaternion, Q, as N-by-4 numeric matrix, NUMERICMATRIX, where each row
+% %   is one quaternion. The input quaternion, Q, is expected to be either
+% %   N-by-4 numeric matrix of row quaternions, or N-by-1 or 1-by-N vector of
+% %   QUATERNION objects. FUNC_NAME and VAR_NAME are used in
+% %   VALIDATEATTRIBUTES to construct the error id and message.
+% 
+% %   Copyright 2017-2019 The MathWorks, Inc.
+% 
+% %#codegen
+% 
+% if (isa(q, 'quaternion'))
+%         
+%     % Extract quaternion parts
+%     [w,x,y,z] = parts(q);
+%     
+%     % Validate that the input quaternion is non-empty and that the input is
+%     % a vector. The validateattributes function is not supported for
+%     % quaternion input. Therefore, the validateattributes is called on each
+%     % part separately.
+%     validateattributes(w, {'numeric'}, {'nonempty', 'vector'}, funcname, varname);
+%     validateattributes(x, {'numeric'}, {'nonempty', 'vector'}, funcname, varname);
+%     validateattributes(y, {'numeric'}, {'nonempty', 'vector'}, funcname, varname);
+%     validateattributes(z, {'numeric'}, {'nonempty', 'vector'}, funcname, varname);
+%     
+%     % If the quaternion input is a row vector, transpose it to get a column
+%     % vector
+%     if (isrow(w))
+%         w = w';
+%         x = x';
+%         y = y';
+%         z = z';
+%     end
+%     
+%     % Save quaternion parts as N-by-4 numeric matrix
+%     numericMatrix = [w,x,y,z];
+%     
+% else
+%     
+%     % Validate that the input is N-by-4 numeric matrix
+%     robotics.internal.validation.validateNumericMatrix(q, funcname, varname, ...
+%         'ncols', 4);
+%     
+%     % Return the input as is
+%     numericMatrix = q;
+%     
+% end
+% 
+% end
+%% 
+% robotics.internal.normalizeRows file
 
 % Normalize and transpose the quaternions
-q = robotics.internal.normalizeRows(q).';
+% q = robotics.internal.normalizeRows(q).';
+
+%from  normalize rows in robotics tool box
+
+q= bsxfun(@times, q, 1./sqrt(sum(q.^2,2)));
+
+q = q';
+
+
+%% 
 
 % Reshape the quaternions in the depth dimension
 q2 = reshape(q,[4 1 size(q,2)]);
@@ -89,7 +101,14 @@ R = permute(R, [2 1 3]);
 %% From rotmto tfrorm
 
 % Ortho-normality is not tested, since this validation is expensive
-robotics.internal.validation.validateRotationMatrix(R, 'rotm2tform', 'R');
+% function validateRotationMatrix(R, funcname, varname, varargin)
+% robotics.internal.validation.validateRotationMatrix(R, 'rotm2tform', 'R');
+
+%Checking if single double real 3d size... not sure what line 110 does
+%since 2014 doesn't recognize rotm2tofrm
+validateattributes(R, {'single','double'}, {'nonempty', ...
+    'real','3d','size',[3 3 NaN],}, ...
+    'rotm2tform', 'R'); 
 
 numMats = size(R,3);
 
