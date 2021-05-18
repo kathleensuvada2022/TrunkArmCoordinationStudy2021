cde2a9c7bb349b0c8ea437be6ae869f1dbec8d38
Kathleen Suvada <kathleensuvada2022@u.northwestern.edu>
2021-05-18 17:39:39 -0500

    Created script for looking at all raw EMGS
    Created script to grab ACT3d Dist and velocity data. Look at emgs (all) at various points of reach

---
+++ DataAnalysis/EMGtestscript.m
@@ -0,0 +1,44 @@
+%% Loads in EMGS, normalizes and rectifies 
+% For RTIS2003 Paretic limb
+
+
+
+filepath = '/Users/kcs762/OneDrive - Northwestern University/TACS/Data';
+partid = '/RTIS2003/Left42321';
+
+maxes = load([filepath partid '/' 'Maxes' '/' 'maxEMG.mat']);
+maxes = maxes.maxEMG;
+load([filepath partid '/' 'RTIS2003_setup_Final_TR.mat']); %loading set up 
+
+% 1-TRtable 2-TR 25%  3-TR 50% 4-TUtable 5-TU 25% 6-TU 50% 
+ExpCond = 3;
+
+trials = setup.trial{1,ExpCond};
+
+for i = 1:length(trials)
+
+    load([filepath partid '/' 'trial' num2str(trials(i))])
+    
+    trials(i) %showing trial we are on
+
+    emg = data.daq{1,2};
+    emg = emg(:,1:15);
+    emg = emg./maxes; %normalizing
+    
+    
+    t = data.daq{1,1}; %loading in the time vector
+    
+    
+    actdata=data.act;
+    
+    %Plotting Distance and Velocity of Trial 
+    [dist,vel,timestart,timevelmax,timeend,timedistmax]=ComputeReachStart_NRSA(actdata) 
+    
+    %Plot in the call for plot all EMGS
+    PlotAllEMGs(emg,t,timestart,timevelmax,timeend,timedistmax)
+    
+    
+    
+    pause 
+    
+end 
