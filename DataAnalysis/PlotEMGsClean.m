b3d2f40961487f4adc57c000dda4f2d1a2a68285
Kathleen Suvada <kathleensuvada2022@u.northwestern.edu>
2021-07-07 16:39:17 -0500

    updates to EmgCleanedData

--- DataAnalysis/PlotEMGsClean.m
+++ DataAnalysis/PlotEMGsClean.m
@@ -17,27 +17,33 @@ nEMG=length(idx1);
 memg=max([max(emg(:,idx1));[max(emg(:,idx2)) 0]]);
 yspacing=cumsum([0 memg(2:nEMG)+.9]);
 lax1 = axes('position',[0.07,0.05,0.4,0.6]);
-%set(lax1,'color','none','xgrid','off','ygrid','off','box','off','TickLabelInterpreter','none')
+set(lax1,'color','none','xgrid','off','ygrid','off','box','off','TickLabelInterpreter','none')
 set(lax1,'YTick',fliplr(-yspacing),'YTickLabel',fliplr(emgchan(idx1)),...
     'YLim',[-yspacing(end) memg(1)])%'XTick',[],'XTickLabel',[],'FontSize',14)
 line(lax1,t,emg(:,idx1)-yspacing(ones(length(t),1),:))
 hold on
-line(lax1,'Color','b','Xdata',[.5 .5],'Ydata',[-0.1 .4], 'LineWidth',.7); % start reach
+yl=ylim; %getting the ylimits to plot vertical line
+line(lax1,'Color','b','Xdata',[timestart timestart],'Ydata',[yl(1) yl(2)], 'LineWidth',.7); % start reach
+line(lax1,'Color','m','Xdata',[timevelmax timevelmax],'Ydata',[yl(1) yl(2)],'LineWidth',.7); % max vel
+line(lax1,'Color','c','Xdata',[timedistmax timedistmax],'Ydata',[yl(1) yl(2)],'LineWidth',.7); %max, dist
+line(lax1,'Color','g','Xdata',[timeend timeend],'Ydata',[yl(1) yl(2)],'LineWidth',.7); %endreach
 co=get(lax1,'ColorOrder');
 set(lax1,'ColorOrder',co(end-1:-1:1,:))
 line(lax1,t,meanEMG(:,idx1)-yspacing(ones(length(t),1),:),'LineWidth',2)
 
 lax2 = axes('position',[0.55,0.05,0.4,0.6]);
+yl2=ylim; %getting the ylimits to plot vertical line
+line(lax2,'Color','b','Xdata',[timestart timestart],'Ydata',[yl(1) yl(2)], 'LineWidth',.7); % start reach
+line(lax2,'Color','m','Xdata',[timevelmax timevelmax],'Ydata',[yl(1) yl(2)],'LineWidth',.7); % max vel
+line(lax2,'Color','c','Xdata',[timedistmax timedistmax],'Ydata',[yl(1) yl(2)],'LineWidth',.7); %max, dist
+line(lax2,'Color','g','Xdata',[timeend timeend],'Ydata',[yl(1) yl(2)],'LineWidth',.7); %endreach
 set(lax2,'color','none','xgrid','off','ygrid','off','box','off','TickLabelInterpreter','none')
 set(lax2,'YTick',fliplr(-yspacing(1:7)),'YTickLabel',fliplr(emgchan(idx2)),...
     'YLim',[-yspacing(end) memg(1)])%,'XTick',[],'XTickLabel',[],'FontSize',14)
 line(lax2,t,emg(:,idx2)-yspacing(ones(length(t),1),1:7))
 set(lax2,'ColorOrder',co(end-1:-1:1,:))
 line(lax2,t,meanEMG(:,idx2)-yspacing(ones(length(t),1),1:7),'LineWidth',2)
-%Adding Vertical Line Time points 
-line('Color','m','Xdata',[timevelmax timevelmax],'Ydata',[-0.1 .4],'LineWidth',.7); % max vel
-line('Color','c','Xdata',[timedistmax timedistmax],'Ydata',[-0.1 .4],'LineWidth',.7); %max, dist
-line('Color','g','Xdata',[timeend timeend],'Ydata',[-0.1 .4],'LineWidth',.7); %endreach
+
 
 % ylabel 'V'
 % title(['EMGs - ' flpath(1:end)],'Interpreter','none')
