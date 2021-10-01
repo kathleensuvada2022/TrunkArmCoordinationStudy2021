fbde6b00b90e4afc9bc1b2d3e329a2f617386f81
AnaMariaAcosta <a-acosta@northwestern.edu>
2021-10-01 15:43:19 -0500

    updating edits

--- DataAnalysis/ComputeEulerAngles.m
+++ DataAnalysis/ComputeEulerAngles.m
@@ -427,6 +427,56 @@ xh = cross(yh,zh);
 h = [xh,yh,zh];
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Function to estimate the center of rotation of the GH joint
+% based on a regression equation of the scapular bony landmarks
+% (AC, AA, TS, AI)
+% ADD REFERENCE AND CHECK THE REGRESSION MODEL TO MATCH THE CSs
+%       input : bl=[ac aa ts ai] (3D coordinates of scapular bony landmarks in
+%                              the scapular CS)
+%               Rsca (scapula rotation matrix)
+%       output: gh (3D coordinates of the center of rotation of the GH
+%                   joint in scapular CS)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function gh=ghest(bl,Rscap)
+% Rsca=asscap(ac,ts,ai); % No longer necessary, Rscap is an input
+Rsca=Rsca*diag([-1 -1 1]); % Flips the x and y axes == 180 degree rotation about z FIGURE OUT IF THIS IS NEEDED
+ 
+Osca=(bl(:,1)+bl(:,2))/2; % Osca=(ac+aa)/2; compute midpoint between AC and AA
+
+% ac=Rsca'*(ac-Osca); No necessary because BLs are already in scapular CS
+% aa=Rsca'*(aa-Osca);
+% ts=Rsca'*(ts-Osca);
+% ai=Rsca'*(ai-Osca);
+
+% Change origin to midpoint between AC and AA
+bl=bl-Osca;
+
+lacaa=norm(bl(:,1)-bl(:,2)); %lacaa=norm(ac-aa);
+lacts=norm(bl(:,1)-bl(:,3)); %lacts=norm(ac-ts);
+lacai=norm(bl(:,1)-bl(:,4)); %lacai=norm(ac-ai);
+ltsai=norm(bl(:,3)-bl(:,4)); %ltsai=norm(ts-ai);
+
+% TWO DIFFERENT REGRESSION EQUATIONS (2 AND 3 ARE THE SAME EXCEPT FOR THE NUMBER OF DECIMALS) WHICH ONE IS BEST????
+% ghrel=[ ...
+%   [  lacaa lacts       ltsai]*[     0.45 -0.28        0.18]';
+%   [1       lacts lacai      ]*[-70        0.73 -0.28      ]';
+%   [        lacts lacai ltsai]*[          -0.51  0.42 -0.38]'];
+
+%   ghrel=[ ...
+%     [1       lacts lacai      ]*[  9.8       -0.399  0.223      ]';
+%     [1       lacts lacai      ]*[-69.6        0.731 -0.277      ]';
+%     [1       lacts lacai      ]*[ -2.7       -0.297  0.065      ]'];
+
+% AMA changed ghrel to gh since the last rotation is not needed.
+  gh=[ ...
+    [1       lacts lacai      ]*[ 10       -0.40  0.22      ]';
+    [1       lacts lacai      ]*[-70        0.73 -0.28      ]';
+    [1       lacts lacai      ]*[ -3       -0.30  0.06      ]'];
+
+%gh=Rsca*ghrel+Osca; Not needed because GH is in scapular CS
+end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 02-08-91 %%
 % Function to estimate the axial rotation of the clavicle and compute
 % the clavicle coordinate system
