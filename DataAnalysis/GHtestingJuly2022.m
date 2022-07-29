be3910f3a32717b112f7732fe9b3c7c56e79b542
Kathleen Suvada <kathleensuvada2022@u.northwestern.edu>
2022-07-29 18:16:59 -0500

    GH

--- DataAnalysis/GHtestingJuly2022.m
+++ DataAnalysis/GHtestingJuly2022.m
@@ -5,10 +5,11 @@
 
 %% Loading in Setup file
 filepath = '/Users/kcs762/Library/CloudStorage/OneDrive-NorthwesternUniversity/TACS/Data';
-partid = 'GHTEST722';
-load(fullfile(filepath,partid,'Left',[partid '_setup.mat'])); %load setup file 
+partid = 'GHTEST722'; % Skeleton Data
+% partid = 'RTIS2002';
+load(fullfile(filepath,partid,'Right',[partid '_setup.mat'])); %load setup file 
 
-arm = 'Left';
+arm = 'Right';
 
 %% Load in BLS for participant- raw data
 
@@ -19,27 +20,25 @@ blmat= BLs{1,2}; %Scapular BLs in MARKER CS
 BlNames = ["AC","AA","TS","AI","PC"];
 BLs_marker = blmat;
 
-
-
 ACidx = find(BlNames=='AC');
 [AC,AA,TS,AI,PC]=deal(blmat(:,ACidx),blmat(:,ACidx+1),blmat(:,ACidx+2),blmat(:,ACidx+3),blmat(:,ACidx+4));
 
-% for RTIS2001- Right AC not in correct position
-if strcmp(partid,'RTIS2001')
-    
-    if strcmp(arm,'Right')
-        AC = PC;
-        
-    end
-end
-
-if strcmp(partid,'RTIS2001')
-    if strcmp(arm,'Right')
-        BlNames = ["AC","AA","TS","AI","AC"];
-        BLs_marker(:,1) =BLs_marker(:,5) ;
-        blmat(:,1) = blmat(:,5);
-    end
-end
+% % for RTIS2001- Right AC not in correct position
+% if strcmp(partid,'RTIS2001')
+%     
+%     if strcmp(arm,'Right')
+%         AC = PC;
+%         
+%     end
+% end
+% 
+% if strcmp(partid,'RTIS2001')
+%     if strcmp(arm,'Right')
+%         BlNames = ["AC","AA","TS","AI","AC"];
+%         BLs_marker(:,1) =BLs_marker(:,5) ;
+%         blmat(:,1) = blmat(:,5);
+%     end
+% end
 
 
 
@@ -53,8 +52,8 @@ ac = AC(1:3);
 %% Compute Distances - confirming PC 
 
 Dist_PC_AC = norm(pc-ac)
-Dist_PC_AA = norm(pc-aa)
-%% Creating Scapular CS
+Dist_PC_AA = norm(pc-aa);
+%% Creating Scapular CS- with PC (Meskers)
 
 xs = (AC(1:3)-TS(1:3)) / norm(AC(1:3)-TS(1:3));
 zhulp = cross(xs,(AC(1:3)-AI(1:3)));
@@ -71,7 +70,23 @@ ScapCoord = [s Orig];
 
 Rsca = ScapCoord(1:3,1:3);
 
+%% Creating Scapular CS - NON PC
 
+xs = (TS(1:3)-AC(1:3)) / norm(TS(1:3)-AC(1:3));
+zhulp = cross(xs,(AC(1:3)-AI(1:3)));
+zs = zhulp/norm(zhulp);
+ ys = cross(zs,xs);
+
+s = [xs,ys,zs];
+
+s = [s;0 0 0];
+
+Orig = AC(1:4);
+
+%Scapular CS in Marker Frame with origin at AC
+ScapCoord = [s Orig];
+
+Rsca = ScapCoord(1:3,1:3);
 %% Plotting BLs and Scap CS
 
 figure(29)
@@ -86,13 +101,11 @@ end
 %Plotting the Scapular Polygon
 plot3([BLs_marker(1,4) BLs_marker(1,3)],[BLs_marker(2,4) BLs_marker(2,3)],[BLs_marker(3,4) BLs_marker(3,3)],'r') % line between AI and TS
 plot3([BLs_marker(1,4) BLs_marker(1,2)],[BLs_marker(2,4) BLs_marker(2,2)],[BLs_marker(3,4) BLs_marker(3,2)],'r') % line between AI and AA
-%plot3([BLs_marker(1,3) BLs_marker(1,2)],[BLs_marker(2,3) BLs_marker(2,2)],[BLs_marker(3,3) BLs_marker(3,2)],'r') % line between TS and AA
 plot3([BLs_marker(1,3) BLs_marker(1,1)],[BLs_marker(2,3) BLs_marker(2,1)],[BLs_marker(3,3) BLs_marker(3,1)],'r') % line between TS and AC
 plot3([BLs_marker(1,1) BLs_marker(1,2)],[BLs_marker(2,1) BLs_marker(2,2)],[BLs_marker(3,1) BLs_marker(3,2)],'r') % line between AC and AA
 
 quiver3(ScapCoord([1 1 1],4)',ScapCoord([2 2 2],4)',ScapCoord([3 3 3],4)',50*ScapCoord(1,1:3),50*ScapCoord(2,1:3),50*ScapCoord(3,1:3))
 text(ScapCoord(1,4)+50*ScapCoord(1,1:3),ScapCoord(2,4)+50*ScapCoord(2,1:3),ScapCoord(3,4)+50*ScapCoord(3,1:3),{'X_S','Y_S','Z_S'})
-% %
 
 plot3(0,0,0,'o')
 text(0,0,0,'Marker','FontSize',14)
@@ -101,27 +114,16 @@ xlabel('X axis (mm)')
 ylabel('Y axis (mm)')
 zlabel('Z axis (mm)')
 
-title('Scap CS Raw Data in Marker CS during digitization','FontSize',16)
-
+title('Scap CS Raw Data in Marker CS','FontSize',16)
 
 
 %% Converting BLS to Bone CS 
 
 Bls_bone_AC = inv(ScapCoord)* blmat;
 
-
-%% For right hand making mimic left 
-% 
-% if strcmp(arm,'Right')
-%     
-% Bls_bone_AC_new = roty(pi)*Bls_bone_AC(1:3,:);
-% 
-% Bls_bone_AC = Bls_bone_AC_new ;
-% 
-% end
 %% Plotting BLs With origin at AC 
 
-figure(30)
+figure(31)
 %Plotting the BonyLandmarks and their Labels
 for i = 1:length(BlNames)
     plot3(Bls_bone_AC(1,i),Bls_bone_AC(2,i),Bls_bone_AC(3,i),'-o','Color','b','MarkerSize',10,...
@@ -144,35 +146,11 @@ ylabel('Y axis (mm)')
 zlabel('Z axis (mm)')
 
 title('Bls in Bone CS during digitization- Origin AC','FontSize',16)
-%% Flipped Z and Y 
-% figure(31)
-% %Plotting the BonyLandmarks and their Labels
-% for i = 1:length(BlNames)
-%     plot3(Bls_bone_AC(1,i),Bls_bone_AC(3,i),Bls_bone_AC(2,i),'-o','Color','b','MarkerSize',10,...
-%         'MarkerFaceColor','#D9FFFF')
-%     hold on
-%     text(Bls_bone_AC(1,i),Bls_bone_AC(3,i),Bls_bone_AC(2,i),BlNames(i),'FontSize',14)
-% end
-% 
-% %Plotting the Scapular Polygon
-% plot3([Bls_bone_AC(1,4) Bls_bone_AC(1,3)],[Bls_bone_AC(3,4) Bls_bone_AC(3,3)],[Bls_bone_AC(2,4) Bls_bone_AC(2,3)],'r') % line between AI and TS
-% plot3([Bls_bone_AC(1,4) Bls_bone_AC(1,2)],[Bls_bone_AC(3,4) Bls_bone_AC(3,2)],[Bls_bone_AC(2,4) Bls_bone_AC(2,2)],'r') % line between AI and AA
-% %plot3([Bls_bone_AC(1,3) Bls_bone_AC(1,2)],[Bls_bone_AC(2,3) Bls_bone_AC(2,2)],[Bls_bone_AC(3,3) Bls_bone_AC(3,2)],'r') % line between TS and AA
-% plot3([Bls_bone_AC(1,3) Bls_bone_AC(1,1)],[Bls_bone_AC(3,3) Bls_bone_AC(3,1)],[Bls_bone_AC(2,3) Bls_bone_AC(2,1)],'r') % line between TS and AC
-% plot3([Bls_bone_AC(1,1) Bls_bone_AC(1,2)],[Bls_bone_AC(3,1) Bls_bone_AC(3,2)],[Bls_bone_AC(2,1) Bls_bone_AC(2,2)],'r') % line between AC and AA
-% 
-% 
-% axis equal
-% xlabel('X axis (mm)')
-% ylabel('Z axis (mm)')
-% zlabel('Y axis (mm)')
-% 
-% title('Bls in Bone CS during digitization- Origin AC','FontSize',16)
-% 
 
 %% BLS in Bone with center at AC
 
 %Order of BLs: ["AC","AA","TS","AI","PC"];
+
  
 ac = Bls_bone_AC(:,1);
 aa = Bls_bone_AC(:,2);
@@ -181,12 +159,7 @@ ai = Bls_bone_AC(:,4);
 pc = Bls_bone_AC(:,5);
 
 
-
-
-%% Feeding into Linear Regression - WITH PC!!!
-
-
-% Original Way 'ComputeEulerAngles' --> 'CalculateGH'
+%% Linear Regression - MESKERS with PC!! 
 
 lacaa=norm(ac(1:3)-aa(1:3));
 ltspc=norm(ts(1:3)-pc(1:3));
@@ -208,10 +181,14 @@ GHz = thz*scz;
 
 gh_b=[GHx;GHy;GHz]; %gh in bone
 
-%%  Linear Regression - NO PC!!
-Rsca=Rsca*diag([-1 -1 1]);
-Osca=(ac+aa)/2;
+%% Linear Regression - NO PC!!
+ Osca=(ac+aa)/2;
 
+ aa= aa-Osca;
+ ac = ac-Osca;
+ ai = ai-Osca;
+ ts = ts-Osca;
+ pc = pc -Osca;
 
 lacaa=norm(ac(1:3)-aa(1:3)); % length from AC to AA
 lacts=norm(ac(1:3)-ts(1:3)); % length from AC to TS
@@ -236,34 +213,52 @@ ltsai=norm(ts(1:3)-ai(1:3)); % length from TS to AI
     [1       lacts lacai      ]*[ -3       -0.30  0.06      ]']; % Z coord
 
 gh_b2 = gh;
-%% 
-%plotting respective GHs in bone CS 
-figure(30)
+%% MESKERS
+%
+figure(31)
 plot3(gh_b(1), gh_b(2),gh_b(3),'*')
-text(gh_b(1), gh_b(2),gh_b(3),'GH comp PC')
+text(gh_b(1), gh_b(2),gh_b(3),'GH PC')
 
-
+%% NON PC MODEL
+figure(32)
 plot3(gh_b2(1), gh_b2(2),gh_b2(3),'*')
-text(gh_b2(1), gh_b2(2),gh_b2(3),'GH comp')
+text(gh_b2(1), gh_b2(2),gh_b2(3),'GH NOPC')
+hold on 
+plot3(pc(1),pc(2),pc(3),'o')
+text(pc(1),pc(2),pc(3),'PC')
+hold on
+plot3(aa(1),aa(2),aa(3),'o')
+text(aa(1),aa(2),aa(3),'AA')
+plot3(ai(1),ai(2),ai(3),'o')
+text(ai(1),ai(2),ai(3),'AI')
+plot3(ac(1),ac(2),ac(3),'o')
+text(ac(1),ac(2),ac(3),'AC')
+plot3(ts(1),ts(2),ts(3),'o')
+text(ts(1),ts(2),ts(3),'TS')
 
+%Plotting the Scapular Polygon
+plot3([ai(1) ts(1)],[ai(2) ts(2)],[ai(3) ts(3)],'r') % line between AI and TS
+plot3([ai(1) aa(1)],[ai(2) aa(2)],[ai(3) aa(3)],'r') % line between AI and AA
+plot3([ts(1) ac(1)],[ts(2) ac(2)],[ts(3) ac(3)],'r') % line between TS and AC
+% plot3([ac(1) aa(1)],[ac(2) aa(2)],[aa(3) ac(3)],'r') % line between AC and AA
+axis equal
+xlabel('X axis (mm)')
+ylabel('Y axis (mm)')
+zlabel('Z axis (mm)')
 
+%% Converting back to Marker CS
 
+gh_b2=(Rsca*gh_b2(1:3))+Osca(1:3); 
+ac=(Rsca*ac(1:3))+Osca(1:3); 
+aa=(Rsca*aa(1:3))+Osca(1:3); 
+ts=(Rsca*ts(1:3))+Osca(1:3); 
+ai=(Rsca*ai(1:3))+Osca(1:3); 
+pc=(Rsca*pc(1:3))+Osca(1:3);
+  
 
-%% For right hand mimicking left rotating back
-% 
-% if strcmp(arm,'Right')
-% 
-% Bls_bone_AC_new = roty(pi)*Bls_bone_AC(1:3,:);
-% 
-% Bls_bone_AC = Bls_bone_AC_new ;
-% 
-% gh_b2 = roty(pi)*gh_b2;
-% 
-% end
-
-%%
-gh_m=(ScapCoord*[gh_b;1]); %yields gh in marker
+%%  GH in Shoulder marker
+gh_m=(ScapCoord*[gh_b;1]); 
 
 figure(29)
-plot3(gh_m(1), gh_m(2),gh_m(3),'*')
-text(gh_m(1), gh_m(2),gh_m(3),'GH Comp')
+ plot3(gh_m(1),gh_m(2),gh_m(3),'o')
+  text(gh_m(1),gh_m(2),gh_m(3),'GH_Meskers')
