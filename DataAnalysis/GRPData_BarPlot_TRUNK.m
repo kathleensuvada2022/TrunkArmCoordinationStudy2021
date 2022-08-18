e9018f5b56393f56f2c9004a2a400efea67cddc8
Kathleen Suvada <kathleensuvada2022@u.northwestern.edu>
2022-08-18 16:28:15 -0500

    Group data

--- DataAnalysis/GRPData_BarPlot_TRUNK.m
+++ DataAnalysis/GRPData_BarPlot_TRUNK.m
@@ -15,17 +15,17 @@ hold on
 
 NParticipants = length(Individ_Means_1); %Gives Number of Participants 
 %% Controls
-p1 =errorbar([5 6],[TD_Cond3 TD_Cond6],[SE_Cond3 SE_Cond6],'k','LineWidth',2); % Table restrained vs unrestrained Controls
-p2 = plot(5,Individ_Means_3,'s','Color','b','Linewidth',2);
-p3 = plot(6,Individ_Means_6,'s','Color','b','Linewidth',2);
+p1 =errorbar([5 6],[TD_Cond2 TD_Cond5],[SE_Cond2 SE_Cond5],'k','LineWidth',2); % Table restrained vs unrestrained Controls
+p2 = plot(5,Individ_Means_2,'s','Color','b','Linewidth',2);
+p3 = plot(6,Individ_Means_5,'s','Color','b','Linewidth',2);
 %% Stroke - Paretic
-p4 =errorbar([1 2],[TD_Cond3 TD_Cond6],[SE_Cond3 SE_Cond6],'k','LineWidth',2); % Table restrained vs unrestrained Controls
-p5 = plot(1,Individ_Means_3,'s','Color',[0.4940 0.1840 0.5560],'Linewidth',2);
-p6 = plot(2,Individ_Means_6,'s','Color',[0.4940 0.1840 0.5560],'Linewidth',2);
+p4 =errorbar([1 2],[TD_Cond2 TD_Cond5],[SE_Cond2 SE_Cond5],'k','LineWidth',2); % Table restrained vs unrestrained Controls
+p5 = plot(1,Individ_Means_2,'s','Color',[0.4940 0.1840 0.5560],'Linewidth',2);
+p6 = plot(2,Individ_Means_5,'s','Color',[0.4940 0.1840 0.5560],'Linewidth',2);
  %% Stroke - Non Paretic
-p7 =errorbar([3 4],[TD_Cond3 TD_Cond6],[SE_Cond3 SE_Cond6],'k','LineWidth',2); % Table restrained vs unrestrained Controls
-p8 = plot(3,Individ_Means_3,'s','Color',[0.9290 0.6940 0.1250],'Linewidth',2);
-p9 = plot(4,Individ_Means_6,'s','Color',[0.9290 0.6940 0.1250],'Linewidth',2);
+p7 =errorbar([3 4],[TD_Cond2 TD_Cond5],[SE_Cond2 SE_Cond5],'k','LineWidth',2); % Table restrained vs unrestrained Controls
+p8 = plot(3,Individ_Means_2,'s','Color',[0.9290 0.6940 0.1250],'Linewidth',2);
+p9 = plot(4,Individ_Means_5,'s','Color',[0.9290 0.6940 0.1250],'Linewidth',2);
 
 
 
@@ -36,8 +36,8 @@ ax=gca;
 xticklabels({' ', 'Stroke (P)-REST', 'Stroke (P)-UNREST','Stroke (NP)-REST', 'Stroke (NP)-UNREST','Controls-REST' ,'Controls-UNREST'})
 ax.XAxis.FontSize = 20;
 ax.YAxis.FontSize = 20;
-ylim([0 10])
-title('Trunk Displacement-50 %','FontSize',28)
+ylim([0 6])
+title('Trunk Displacement-25% MVT','FontSize',28)
 ylabel('Trunk Displacement (%LL)','FontSize',20)
 
 
@@ -45,7 +45,7 @@ ylabel('Trunk Displacement (%LL)','FontSize',20)
 %% Wait until have Controls,Stroke P and Stroke NP
 h = [p2(1);p5(1);p8(1)];
 % legend(p5(1), 'Stroke-Paretic','FontSize',16)
-legend(h,'Controls','Stroke:Paretic','Stroke:Non-Paretic','FontSize',16)
+legend(h,'Controls','Stroke:Paretic','Stroke:Non-Paretic','FontSize',24)
 
 end
 
