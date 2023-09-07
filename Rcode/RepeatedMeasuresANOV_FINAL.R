4592c55b5c89cdfca38428b5364b67ac3569064f
Kathleen Suvada <kathleensuvada2022@u.northwestern.edu>
2023-09-07 15:59:23 -0500

    Update RepeatedMeasuresANOV_FINAL.R

--- Rcode/RepeatedMeasuresANOV_FINAL.R
+++ Rcode/RepeatedMeasuresANOV_FINAL.R
@@ -137,6 +137,13 @@ library(rstatix)
 
 
 ############# RUN BELOW FOR PARETIC LIMB####################### FINAL MAY 2023
+
+# For Paretic
+AllData_Stroke_Paretic_R_AVGs$Restraint = as.factor(AllData_Stroke_Paretic_R_AVGs$Restraint)
+AllData_Stroke_Paretic_R_AVGs$Loading = as.factor(AllData_Stroke_Paretic_R_AVGs$Loading)
+AllData_Stroke_Paretic_R_AVGs$ID = as.factor(AllData_Stroke_Paretic_R_AVGs$ID)
+
+
 res.aov <- anova_test(
   data = AllData_Stroke_Paretic_R_AVGs, dv = RDLL, wid = ID,
   within = c(Restraint,Loading)
@@ -158,6 +165,11 @@ one.way <- AllData_Stroke_Paretic_R_AVGs %>%
 one.way 
 
 ############# RUN BELOW FOR NON-PARETIC LIMB####################### FINAL MAY 2023
+# For Non-Paretic
+AllDataNonParetic_R_AVGs$Restraint = as.factor(AllDataNonParetic_R_AVGs$Restraint)
+AllDataNonParetic_R_AVGs$Loading = as.factor(AllDataNonParetic_R_AVGs$Loading)
+AllDataNonParetic_R_AVGs$ID = as.factor(AllDataNonParetic_R_AVGs$ID)
+
 res.aov <- anova_test(
   data = AllDataNonParetic_R_AVGs, dv = RDLL, wid = ID,
   within = c(Restraint,Loading)
@@ -185,6 +197,7 @@ one.way
 AllData_Controls_R_AVGs$Restraint = as.factor(AllData_Controls_R_AVGs$Restraint)
 AllData_Controls_R_AVGs$Loading = as.factor(AllData_Controls_R_AVGs$Loading)
 AllData_Controls_R_AVGs$ID = as.factor(AllData_Controls_R_AVGs$ID)
+
 res.aov <- anova_test(
   data = AllData_Controls_R_AVGs, dv = RDLL, wid = ID,
   within = c(Restraint,Loading)
