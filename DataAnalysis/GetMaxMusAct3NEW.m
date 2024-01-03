09b6f34a23266783aaf2ab11902d866d14392431
Kathleen Suvada <kathleensuvada2022@u.northwestern.edu>
2024-01-03 09:49:05 -0600

    Update GetMaxMusAct3NEW.m

--- DataAnalysis/GetMaxMusAct3NEW.m
+++ DataAnalysis/GetMaxMusAct3NEW.m
@@ -1,16 +1,8 @@
-%%% USED JULY 2021 WORKS!
+%%% Edited Dec 2023
 
 %%
-% flpath='/Users/kcs762/OneDrive - Northwestern University/TACS/Data/RTIS2010/Left/Maxes';
-% basename='trial';
-% partid='RTIS2010';
-% setfname='RTIS2010_setup';
-% 
-% plotflag=1;
-% hand ='Left';
-%%
 
-function maxEMG=GetMaxMusAct3(flpath,basename,setfname,partid,plotflag,hand)
+function maxEMG=GetMaxMusAct3NEW(flpath,basename,setfname,partid,plotflag,hand)
 % Function to get the maximum EMGs from the MVC Torques data. The output is a .mat file (*MaxEMG.mat)
 % which contains the following matrices:
 % Inputs: basename: triacleal file name (before index)
@@ -20,6 +12,7 @@ function maxEMG=GetMaxMusAct3(flpath,basename,setfname,partid,plotflag,hand)
 %          maxidx: [1 x nEMG]. Trial index where max EMG occurred
 % 7/19 AMA - updated routine for Kacey's trunk-arm reaching data
 % Updated 10.2019
+% Dec 2023
 % Usage example:
 %    [maxEMG]=GetMaxMusAct('/Users/kcs762/Documents/MATLAB/Data/AIM1/MaxEMGS/owen061819maxemgs/Left','max','savedsetupKacey',1)
 
@@ -39,21 +32,21 @@ maxTEMG=zeros(length(trials),nEMG);
 Tlength=zeros(length(trials),1);
 %%
 for j=1:length(trials)
-    disp([flpath trials(j).name])
-    
+    disp([flpath 'maxes' num2str(j)])
+
     %Replace this line with however you load your data. Important to
-    %include the code that selects the jth file     
-    
+    %include the code that selects the jth file
+
     try load([flpath basename num2str(j)]);
-%         data= Totaldata.data %updated 10.2019
-    catch maxTEMG(j,:)=zeros(1,nEMG); continue % make comment about error after catch 
+        %         data= Totaldata.data %updated 10.2019
+    catch maxTEMG(j,:)=zeros(1,nEMG); continue % make comment about error after catch
     end
-%     Tlength(j)=length(data);
+    %     Tlength(j)=length(data);
 
     emg=detrend(data(:,1:15)); %updated 12.2023- using raw not filtered maxes
     % Rectify EMG
     emg=abs(emg);
- 
+
     emg = emg-repmat(mean(emg(1:250,:)),length(emg),1); %removes baseline
 
     % Compute the mean EMG
@@ -62,32 +55,31 @@ for j=1:length(trials)
     % Find maximum EMG
     [maxTEMG(j,:),maxtidx(j,:)]=max(meanEMG);
 
-    % ARTIFACT CORRECTION
 
-
-if 1 %plotres
-
+    if 0 %plotting each trial j = trial num
+        figure(2)
+clf
         t=(0:length(emg) - 1)/sampRate;
 
-            clf
-            title(['Maximum EMGs -  (trial' num2str(j) '): '])
-            memg=max(emg);
-            yspacing=cumsum([0 emg(2:nEMG)+0.1]);
-            rax = axes('position',[0.1,0.05,0.75,0.9]);
-            set(rax,'YAxisLocation','right','color','none','xgrid','off','ygrid','off','box','off');
-%             set(rax,'YTick',fliplr(-yspacing),'YTickLabel',fliplr(ylabels),...
-%                 'YLim',[-yspacing(end) memg(1)],'XTick',[],'XTickLabel',[])
-            set(rax,'YTick',fliplr(-yspacing),'YTickLabel',flipud(strcat(emgchan')),...
-                 'YLim',[-yspacing(end) memg(1)],'XTick',[],'XTickLabel',[])
-            lax=axes('position',[0.1,0.05,0.75,0.9]);
-            plot(t,emg-yspacing(ones(length(t),1),:),t(maxtidx(j,:)),maxTEMG(j,:)-yspacing,'k*'),hold on;
-%             line([t([upid,upid]) t([dnid,dnid])],repmat([-yspacing(end) memg(1)]',1,2),'Color','b')
-            co=get(lax,'ColorOrder');
-            set(lax,'ColorOrder',co([2:end 1],:),'YLim',[-yspacing(end) memg(1)])
-            plot(t,meanEMG-yspacing(ones(length(t),1),:),'LineWidth',2)
-            ylabel 'V'
-%             pause
-        end
+        rax = axes('position',[0.1,0.05,0.75,0.9]);
+        set(rax,'YAxisLocation','right','color','none','xgrid','off','ygrid','off','box','off');
+
+        title(['Maximum EMGs -' basename num2str(j)],'FontSize',24)
+        memg=max(emg);
+        yspacing=cumsum([0 emg(2:nEMG)+0.1]);
+        %             set(rax,'YTick',fliplr(-yspacing),'YTickLabel',fliplr(ylabels),...
+        %                 'YLim',[-yspacing(end) memg(1)],'XTick',[],'XTickLabel',[])
+        set(rax,'YTick',fliplr(-yspacing),'YTickLabel',flipud(strcat(emgchan')),...
+            'YLim',[-yspacing(end) memg(1)],'XTick',[],'XTickLabel',[])
+        lax=axes('position',[0.1,0.05,0.75,0.9]);
+        plot(t,emg-yspacing(ones(length(t),1),:),t(maxtidx(j,:)),maxTEMG(j,:)-yspacing,'k*'),hold on;
+        %             line([t([upid,upid]) t([dnid,dnid])],repmat([-yspacing(end) memg(1)]',1,2),'Color','b')
+        co=get(lax,'ColorOrder');
+        set(lax,'ColorOrder',co([2:end 1],:),'YLim',[-yspacing(end) memg(1)])
+        plot(t,meanEMG-yspacing(ones(length(t),1),:),'LineWidth',2)
+        ylabel 'V'
+        %             pause
+    end
 end
 % %%
 
@@ -97,49 +89,40 @@ Tlength(j)=size(data,1);
 maxTlength=max(Tlength);
 
 
-% %% Plots 
+%% Grabbing the trials where the max occurs per muscle
 if plotflag
-    %figure(1), clf
-    newemg=zeros(length(data),nEMG);  
+    newemg=zeros(length(data),nEMG);
     t=(0:maxTlength - 1)/sampRate;
     for k=1:nEMG
-        load([flpath basename num2str(j)]);
-%         newemg(:,k)=[data(:,k+6);zeros(maxTlength-length(data),1)];
-%         data=structData.totalData; % no longer need this line 10.2019
-        newemg(:,k)=data(:,k);  
+        load([flpath basename num2str(maxidx(k))]);
+%         load([flpath trials(maxidx(k)).name]);
+        newemg(:,k)=data(:,k);  % Each column is muscle and rows are whole trial over time where max occurs
     end
 end
-   % PlotEMGs(newemg)
-    figure(), clf
-    newemg=abs(detrend(newemg));
-    newemg = newemg-repmat(mean(newemg(1:250,:)),length(newemg),1); %removes baseline
-    newmeanEMG=movmean(newemg,ds);
-    memg=max(newemg);
-    yspacing=cumsum([0 memg(2:nEMG)+.1]);
-    rax = axes('position',[0.1,0.05,0.75,0.9]);
-    set(rax,'YAxisLocation','right','color','none','xgrid','off','ygrid','off','box','off');
-    set(rax,'TickLabelInterpreter','none')
-    set(rax,'YTick',fliplr(-yspacing),'YTickLabel',flipud(strcat(emgchan','-',strvcat(maxidx))),...
-        'YLim',[-yspacing(end) memg(1)],'XTick',[],'XTickLabel',[])
-    lax=axes('position',[0.1,0.05,0.75,0.9]);
-   % plot(t,newemg-yspacing(ones(length(t),1),:))% t(diag(maxtidx(maxidx,1:nEMG)),maxEMG-yspacing,'k*'),hold on;
-   %plot(t,emg-yspacing(ones(length(t),1),:),t(maxtidx(j,:)),maxTEMG(j,:)-yspacing,'k*'),hold on;
-
-   plot(t,newemg-yspacing(ones(length(t),1),:),t(diag(maxtidx(maxidx,1:nEMG))),maxEMG-yspacing,'k*'),hold on;
-    co=get(lax,'ColorOrder');
-    set(lax,'ColorOrder',co(end-1:-1:1,:),'YLim',[-yspacing(end) memg(1)])
-    plot(t,newmeanEMG-yspacing(ones(length(t),1),:),'LineWidth',2)
-%     alltimesinsec = t(maxtidx(maxidx),1);
-%     allmaxes = newmeanEMG(maxtidx(maxidx),:);
- %   plot(alltimesinsec(1),allmaxes(1)-yspacing(ones(length(maxtidx(maxidx)),1),1),'k*')
-    ylabel 'V'
-    title(['Maximum EMGs across all trials - ' flpath(1:end)],'Interpreter','none')
-    print('-f1','-djpeg',[flpath '\MaxEMGs'])
+%% Plotting the trial where the max occurs per Muscle
+figure(1), clf
+newemg=abs(detrend(newemg));
+newemg = newemg-repmat(mean(newemg(1:250,:)),length(newemg),1); %removes baseline
+newmeanEMG=movmean(newemg,ds);
+memg=max(newemg);
+yspacing=cumsum([0 memg(2:nEMG)+.1]);
+rax = axes('position',[0.1,0.05,0.75,0.9]);
+set(rax,'YAxisLocation','right','color','none','xgrid','off','ygrid','off','box','off');
+set(rax,'TickLabelInterpreter','none')
+set(rax,'YTick',fliplr(-yspacing),'YTickLabel',fliplr(strcat(emgchan,'-maxes',cellstr(string(maxidx)))),...
+    'YLim',[-yspacing(end) memg(1)],'XTick',[],'XTickLabel',[])
+lax=axes('position',[0.1,0.05,0.75,0.9]);
+plot(t,newemg-yspacing(ones(length(t),1),:),t(diag(maxtidx(maxidx,1:nEMG))),maxEMG-yspacing,'k*'),hold on;
+co=get(lax,'ColorOrder');
+set(lax,'ColorOrder',co(end-1:-1:1,:),'YLim',[-yspacing(end) memg(1)])
+plot(t,newmeanEMG-yspacing(ones(length(t),1),:),'LineWidth',2)
+ylabel 'V'
+title(['Maximum EMGs across all trials - ' flpath(1:end)],'Interpreter','none')
+print('-f1','-djpeg',[flpath '\MaxEMGs'])
 
 
 % Save results
-% save([flpath '/maxEMG'],'maxEMG','maxidx')
+ save([flpath '/maxEMG'],'maxEMG','maxidx')
 
-end % if plotflag
-
+end
 
